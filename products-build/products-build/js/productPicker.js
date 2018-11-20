be68663ea76887919ad9e509a599304462aff5d4
Derek Will <dwill_85@hotmail.com>
2018-11-20 16:18:48 -0500

    AJAX call working.

--- products-build/products-build/js/productPicker.js
+++ products-build/products-build/js/productPicker.js
@@ -1,33 +1,47 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
+
+    // products AJAX call
+    var productsURL = 'JSON/products-test.json';
+
+    $.get(productsURL, function(data) {
+        // parse json
+        data = JSON.parse(data);
+
+        // iterate over json data
+        $.each(data.rows, function(index, value) {
+            console.log(value.id);
+        });
+    }); // end AJAX call
+
+
+
     // storing common HTML elements
     var productCategory = $('.product-category');
+    var categoryTitle = productCategory.find('.prodcat-picker');
     var productOptions = $('.prodcat-options');
 
     // hide all product category selection sections
     var allCategoryOptions = productCategory.find(productOptions);
 
+    // build category selection currently clicked
+
     
     // click trigger to operate accordion
-    $(productCategory).click(function () {
+    $(categoryTitle).click(function() {
         // grab HTML id
-        var categoryTitle = productCategory.find('.prodcat-picker');
-        var categoryID = $(this).find(categoryTitle).attr('id');
+        var categoryID = $(this).attr('id');
 
-        // instantiate switch case variables
-        var categoryIDelement;
-
-        //// hide all opened product category options
-        //$(allCategoryOptions).hide();
-
-        // show or hide specific category option
-        categoryIDelement = $('#' + categoryID);
-        // get the sibling .prodcat-options class related to category's title
+        // build jquery categoryID title selector
+        var categoryIDelement = $('#' + categoryID);
+        // get the sibling prodcat-options class related to category's title
         var currentCategoryOption = categoryIDelement.next(productOptions);
 
+        // call the accordion functionality
         showOrHide(currentCategoryOption, allCategoryOptions);
 
     }); // end click function
-});
+
+}); // end doc ready
 
 
 
@@ -36,14 +50,10 @@
 function showOrHide(currentSelection, notSelected) {
     // show product category options
     if (currentSelection.css('display') === "none") {
-
         notSelected.hide();
-
-        currentSelection.show();
-
+        currentSelection.show(2000);
     }
     else {
         currentSelection.hide();
     }
 } // end show and hide 
-
